1be4eaddfb3ea614455eabc35a27420a00401b3a
John <jqm004@alumnos.ucn.cl>
2018-05-14 11:24:25 -0400

    Programa inicial: Code

---
+++ src/main/java/cl/ucn/disc/bigdata/code/Main.java
@@ -0,0 +1,22 @@
+package cl.ucn.disc.bigdata.code;
+
+import org.apache.log4j.Logger;
+
+public final class Main {
+
+    /**
+     * Logger de la clase Main
+     */
+    private static final Logger log = Logger.getLogger(Main.class);
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+
+        // Mensaje de debug
+        log.debug("Hola mundo");
+
+    }
+}
